4ed62cf1cd3922e4269eba539dbe01dbf3782902
Manuel Lopez <manny@chartbeat.com>
2015-07-15 17:21:24 -0400

    modified controllers to pass messages

--- src/luminus_tutorial/routes/home.clj
+++ src/luminus_tutorial/routes/home.clj
@@ -1,17 +1,40 @@
 (ns luminus-tutorial.routes.home
   (:require [luminus-tutorial.layout :as layout]
-            [compojure.core :refer [defroutes GET]]
+            [compojure.core :refer [defroutes GET POST]]
             [ring.util.http-response :refer [ok]]
-            [clojure.java.io :as io]))
-
-(defn home-page []
+            [clojure.java.io :as io]
+            [luminus-tutorial.db.core :as db]
+            [bouncer.core :as b]
+            [bouncer.validators :as v]
+            [ring.util.response :refer [redirect]]))
+;;
+(defn home-page [{:keys [flash]}]
   (layout/render
-    "home.html" {:docs (-> "docs/docs.md" io/resource slurp)}))
+    "home.html" 
+    (merge {:messages (db/get-messages)}
+           (select-keys flash [:name :message :errors]))))
+
+(defn save-message! [{:keys [params]}]
+  (if-let [errors (validate-message params)]
+    (-> (redirect "/")
+        (assoc :flash (assoc params :errors errors)))
+    (do
+      (db/save-message! (assoc params :timestamp (java.util.Date.)))
+      (redirect "/"))))
 
 (defn about-page []
   (layout/render "about.html"))
 
 (defroutes home-routes
-  (GET "/" [] (home-page))
-  (GET "/about" [] (about-page)))
+  (GET "/" request (home-page))
+  (POST "/" request (save-message! request))
+  (GET "/about" request (about-page)))
+
+(defn validate-message [params]
+  (first
+    (b/validate
+      params
+      :name v/required
+      :message [v/required [v/min-count 10]])))
+
 
